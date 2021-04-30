ed9c6aed8909ff282b5e1863c2c1a127459babba
euguroglu <e.uguroglu@yahoo.com>
2021-05-01 02:01:11 +0300

    dag file column type for hive table changed

--- dag_file.py
+++ dag_file.py
@@ -55,7 +55,7 @@ with DAG("ecommerce_platform",start_date=datetime(2021, 1, 1),
                     `start` TIMESTAMP,
                     `end` TIMESTAMP,
                     source STRING,
-                    source_number INTEGER
+                    source_number LONG
                     )
                 ROW FORMAT DELIMITED
                 FIELDS TERMINATED BY ','
